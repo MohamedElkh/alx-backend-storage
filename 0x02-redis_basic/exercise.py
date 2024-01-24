3df8dd3975ecaaa90f97bef9845bbfe9055d5953
mohamed <mohamedkharashy27@yahoo.com>
2024-01-24 08:49:41 -0800

    1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -82,30 +82,23 @@ class Cache:
 
         return data
 
-    def replay(fn: Callable):
-        """display the history of calls of a particular function"""
-        r = redis.Redis()
-        function_name = fn.__qualname__
-        value = r.get(function_name)
-        try:
-            value = int(value.decode("utf-8"))
-        except Exception:
-            value = 0
+    def replay(method: Callable):
+        """ display the history of calls of a particular function """
+        key = method.__qualname__
 
-        print("{} was called {} times:".format(function_name, value))
-        # inputs = r.lrange(f"{function_name}:inputs", 0, -1)
-        inputs = r.lrange("{}:inputs".format(function_name), 0, -1)
-        # outputs = r.lrange(f"{function_name}:outputs", 0, -1)
-        outputs = r.lrange("{}:outputs".format(function_name), 0, -1)
+        inp = key + ":inputs"
+        outp = key + ":outputs"
 
-        for input, output in zip(inputs, outputs):
-            try:
-                input = input.decode("utf-8")
-            except Exception:
-                input = ""
-            try:
-                output = output.decode("utf-8")
-            except Exception:
-                output = ""
-            # print(f"{function_name}(*{input}) -> {output}")
-            print("{}(*{}) -> {}".format(function_name, input, output))
+        redisx = method.__self__._redis
+        countt = redisx.get(key).decode("utf-8")
+        print("{} was called {} times:".format(key, countt))
+
+        inputlistx = redisx.lrange(inp, 0, -1)
+        outputlistx = redisx.lrange(outp, 0, -1)
+
+        zippedx = list(zip(inputlistx, outputlistx))
+
+        for aa, bb in zippedx:
+            attr, data = aa.decode("utf-8"), bb.decode("utf-8")
+
+            print("{}(*{}) -> {}".format(key, attr, data))
